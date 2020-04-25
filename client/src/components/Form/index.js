3b0cea64b76e577f722e8dd80d65d630a1272cab
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-25 00:40:50 -0700

    created form component for book search

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export function Input(props) {
+  return (
+    <div className="form-group" style={{ marginTop: 30, marginRight: 30, marginBottom: 10 }}>
+      <input className="form-control" {...props} />
+    </div>
+  );
+}
+
+export function FormBtn(props) {
+  return (
+    <button
+      {...props}
+      style={{
+        float: `right`,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 10,
+      }}
+      className="btn btn-primary"
+    >
+      {props.children}
+    </button>
+  );
+}
+FormBtn.propTypes = {
+  children: PropTypes.node,
+};
