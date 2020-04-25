edd85a436388588722e54b70778db516326a798c
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-25 00:42:57 -0700

    updated book model to include id and subtitle

--- models/book.js
+++ models/book.js
@@ -3,9 +3,11 @@ const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+    id: { type: String, required: true },
     title: { type: String, required: true },
+    subtitle: { type: String },
     author: { type: String, required: true },
-    description: { type: String },
+    description: { type: String, required: true },
     image: { type: String, required: true },
     link: { type: String, required: true }
 });
