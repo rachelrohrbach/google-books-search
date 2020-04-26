8d3519e3d11bb7abd8e036a972787e48f962b00f
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-25 18:14:57 -0700

    fixed mongo/model id issue

--- models/book.js
+++ models/book.js
@@ -3,9 +3,7 @@ const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    id: { type: String, required: true },
     title: { type: String, required: true },
-    subtitle: { type: String },
     author: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
