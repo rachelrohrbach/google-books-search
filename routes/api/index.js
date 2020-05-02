f90eeb8f185c4b5f1405205c32ae68ac45c9d826
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-05-02 16:21:01 -0700

    added googlebooks api route to router

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require(`express`).Router();
 const bookRoutes = require(`./books`);
+const googleBookRoutes = require('./googleBooks');
 
 router.use(`/books`, bookRoutes);
+router.use(`/googleBooks`, googleBookRoutes);
 
 module.exports = router;
