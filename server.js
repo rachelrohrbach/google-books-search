08e4ff97203a8c556102b2e0d4aae3b3e5371cba
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-11-10 22:11:38 -0800

    updated mongo connection for heroku config

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === `production`) {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/reactreadinglist`, {
+mongoose.connect(process.env.MONGO_CONNECTION || `mongodb://localhost/reactreadinglist`, {
 useUnifiedTopology: true,
 useNewUrlParser: true,
 useCreateIndex: true
